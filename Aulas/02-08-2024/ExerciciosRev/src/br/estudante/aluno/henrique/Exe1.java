e9d63f600e57c0d90469e6c25c14bb2a0ed4bac8
Henrique Gabriel <henriquegb152019@gmail.com>
2024-08-08 17:36:52 -0300

    Funcionando mais ou menos

--- Aulas/02-08-2024/ExerciciosRev/src/br/estudante/aluno/henrique/Exe1.java
+++ Aulas/02-08-2024/ExerciciosRev/src/br/estudante/aluno/henrique/Exe1.java
@@ -1,24 +1,52 @@
 package br.estudante.aluno.henrique;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Exe1 {
+	
+	
+	public static boolean resposta(String entrada) {
+		 Stack<Character> pilha = new Stack<>();
+
+	     for (char ch : entrada.toCharArray()) {
+	    	 if (ch == '(' || ch == '{' || ch == '[') {
+	    		 pilha.push(ch);
+	         }
+	        
+	        else if (ch == ')' || ch == '}' || ch == ']') {
+	        	if (pilha.isEmpty()) {
+	        		return false;
+	            }
+	                
+	            char topo = pilha.pop();
+	            if ((ch == ')' && topo != '(') ||
+	               (ch == '}' && topo != '{') ||
+	               (ch == ']' && topo != '[')) {
+	            	return false;
+	            }
+	         }
+	       }
+	        
+	       return pilha.isEmpty();	
+	}
 
 	public static void main(String[] args) {
-		String valor = null;
-		boolean resultado = false;
+		Scanner impressora = new Scanner(System.in);
+		String entrada;
+		boolean resultado;
 		
-		valor = JOptionPane.showInputDialog(valor);
+		System.out.println("Entre com um valor com parenteses, colchetes ou chaves:");
+		entrada = impressora.next();
 		
-		if(valor.contains("()")) {
-			resultado = true;
-		}else {
+		resultado = resposta(entrada);
+		
+		System.out.println("A String tá balanceada ? " + resultado);
+		
+		impressora.close();
 			
-		}
-		
-		
-		
-		
 	}
+	
+	
 
 }
